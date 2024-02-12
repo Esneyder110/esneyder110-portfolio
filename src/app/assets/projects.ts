39cf8424275e279d3cce2c5f82ef367228df9b93
Esneyder Monsalve <esneyder110@gmail.com>
2024-02-12 18:27:30 -0400

    feat: add loopstudios landing page project

--- src/app/assets/projects.ts
+++ src/app/assets/projects.ts
@@ -7,6 +7,13 @@ export const projects = [
     srcLive: 'https://quotesapi-dev-zexr.1.us-1.fl0.io/api/v1/docs/#/',
   },
   {
+    title: 'Loopstudios Landing Page',
+    description: 'Landing page, build with React, TypeScript, Vite and Tailwind',
+    imagePreview: '/projects/loopstudios.jpg',
+    srcCode: 'https://github.com/Esneyder110/loopstudios-landing-page',
+    srcLive: 'https://esneyder110.github.io/loopstudios-landing-page/',
+  },
+  {
     title: 'Stop letter generator',
     description: 'A random character generator',
     imagePreview: '/projects/stop.jpg',
